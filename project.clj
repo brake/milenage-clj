fd54ecadf77917cc3e938ae1ed67d2d844cfbab4
Constantin Roganov <rccbox@gmail.com>
2016-01-31 17:06:11 +0300

    [added] *warn-on-reflection* flag [fixed] project repository url

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject org.roganov.milenage "0.1.0-SNAPSHOT"
+(defproject threegpp.milenage-clj "0.1.0"
   :description "3GPP TS 35.206 Milenage algorithm calculation (OPc and all functions)"
-  :url "https://github.com/brake"
+  :url "https://github.com/brake/milenage"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]]
-  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
+  :global-vars {*warn-on-reflection* true}
   :profiles {:uberjar {:aot :all}})
